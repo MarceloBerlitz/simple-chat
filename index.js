7383c4fb1462a886cb512f0c5dc618a70db60463
berlitz <marceloberlitz@outlook.com>
2020-03-05 14:21:17 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('message', message => socket.broadcast.emit('message', message));
+});
+
+app.use(express.static('public'));
+
+const port = 3008;
+server.listen(port, () => {
+    console.log(`listen on port ${port}`)
+});
